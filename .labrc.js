5936cf4591a1471795f9f1fe90695c8d31d01721
Gerardo Lima <gerardo.lima@gmail.com>
2020-10-05 18:27:30 +0100

    chore(api): add test libraries

---
+++ .labrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	// file-matching
+	flat: false,
+	paths: ['./src'],
+	transform: 'node_modules/lab-transform-typescript',
+
+	// ...
+	assert: false,
+	colors: true,
+	coverage: false,
+	globals: '__core-js_shared__,core,global-symbol-property',
+	lint: false,
+	reporter: 'console',
+	shuffle: true,
+	timeout: 4000,
+	verbose: true,
+}
